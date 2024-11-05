e12ef2fe6d77cf7f47f533b31650ec070aa1aa04
Rey Bonan <marcreybonan@gmail.com>
2024-11-05 21:44:46 +0100

    Create Générateur de mots de passe.py

---
+++ Sécurité/Générateur de mots de passe.py
@@ -0,0 +1,8 @@
+#Import des librairies nécessaires
+
+import random
+import string
+
+#On choisit les caractéristique des mots de passe qu'on souhaite générer
+def genmdp(length = x, use_uppercase = True, use_digits = True, use_special_chars = True): 
+  
