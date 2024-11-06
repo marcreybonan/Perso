2ed44bb310a17f438feea30fbcbe452a93cae7e6
Rey Bonan <marcreybonan@gmail.com>
2024-11-06 23:48:35 +0100

    Create Deux.py

---
+++ Training/Deux.py
@@ -0,0 +1,23 @@
+#Part 2 - Les listes 
+
+#On crée une liste simple
+#L'indice d'une liste commence à 0 
+noms = ["Hermione", "Harry", "Severus", "Albus"]
+
+#On peut afficher le contenu d'une liste de plusieurs manières 
+noms[2]
+print(noms[2])
+print(noms)
+
+#On peut même afficher le contenu avec un chiffre négatif 
+noms[-1] 
+
+#Modifications sur une liste 
+#Pour ce faire, il suffit d'ecrire le nom de la liste puis écrire un opérateur avec un point
+#Ainsi on retrouve append, remove, extend, insert, count, index, reverse, pop etc...
+#len affiche la longueur de la liste 
+noms.append("Ron")
+
+
+
+
