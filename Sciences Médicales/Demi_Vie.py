ae4fc092fe5e8893a535c80bbdcabbbdbd0cd3d4
Rey Bonan <marcreybonan@gmail.com>
2024-11-13 18:19:03 +0100

    Update Demi_Vie.py

--- Sciences Médicales/Demi_Vie.py
+++ Sciences Médicales/Demi_Vie.py
@@ -1,12 +1,25 @@
-print("La demi vie d'un medicament correspond au temps necessaire afin que la concentration plasmatique diminue de moitie. Cette demi vie peut etre d une poignee d'heures a des dizaines d'heures, ainsi celle-ci est tres variable d'un medicament a un autre et ce meme au sein d'une famille de medicaments ;
-      par exemple l'Alprazolam et le Bromazepam sont deux benzodiazepines, le premier a une duree de vie moyenne de 11 heures tandis que le second de 17 heures et peut monter a 20 heures. Certains medicaments ayant des effets secondaires pouvant etre dangereux, 
-comme la somnolence au volant notamment, il peut etre utile de connaitre la demi vie d'un medicament ou la concentration plasmatique du medicament absorbe.")
+#On présente le principe de demie-vie d'une substance médicamenteuse en abandonnant les précédentes explications basées sur une classe thérapeutique précise
+print("La demi-vie correspond au temps nécessaire pour que, après l’administration d’un médicament, sa concentration plasmatique diminue de moitié. \n
+  La demi-vie est exprimée en unité de temps et peut varier de quelques minutes à plusieurs semaines selon les médicaments \n
+  La fraction de médicament éliminée en fonction du temps dépend donc de sa demi-vie et l’on considère que la quasi-totalité du médicament est éliminée au bout de 5 demi-vie")
 
-print("Il faut 5 demi-vie afin qu'un médicament soit totalement éliminé") 
+#On met en place une fonction nous permettant de calculer la concentration restante selon plusieurs critères (la demi-vie, la dose absorbée, le temps écoulé)
+def drestante(demi_vie, heure_depuis_prise, dose_initiale):
 
-#On met en place une fonction nous permettant de calculer le pourcentage restant du medicament dans l'organisme de l utilisateur
-def pourcentage(demi_vie, heure_depuis_prise):
-      pourcentage=100*(0.5**(heure_depuis_prise/(demi_vie))
-                       return pourcentage
+      #On calcule le nombre de demi-vies écoulées
+      demie_vie_passees = heure_depuis_prise/demi_vie
 
-#On demande a l utilisateur depuis combien de temps le medicament a ete absorbe ainsi que la demi vie du medicament concerne
+      #On s'intéresse à la concentration restante apres le temps passé
+      crestante = dose_initiale*(0.5**demi_vie)
+      
+      return crestante
+
+#On demande maintenant à l'utilisateur la dose administrée, la demi_vie du medicament et le temps ecoule et on stock ces informations dans une variable flottante 
+def main(): 
+   dose_initiale = float(input("Quelle est la dose consommée en mg ? :"))
+   demi_vie = float(input("Quelle est la demi-vie de la molecule consommée en heures ? :"))
+   heure_depuis_prise = float(input("Quand avez vous consommé la molecule en heure ? :"))
+
+   #On calcule la concentration restante dans l'organisme de l'utilisateur
+   
+   
