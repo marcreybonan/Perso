2c5ca903124ebddda065d72d8fcfac55a4f354fc
Rey Bonan <marcreybonan@gmail.com>
2024-11-19 03:48:34 +0100

    Update liste_eleves.py

--- liste_eleves.py
+++ liste_eleves.py
@@ -1,1 +1,14 @@
-import json
+#Retrouver les listes contenues danss mes anciens téléphones
+
+#On crée la liste principale à laquelle on ajoutera plusieurs petites listes qui se grefferont à celles-ci
+liste_principale = ["Shenali", "Massira", "Victoire", "Tom", "Ariel", "Ali", "Emmanuel", "Solene", "Pierre", "Magalie" "Melissa", "Alexandre", \n
+                    "Charlotte", "Elia", "Gaspard", "Joakim", "Jonas", "Lea",  "Noor", "Oceane", "Zarah", "Lucie", "Sofia", "Sophie", "Vinciane", \n
+                    "Zouhour"]
+
+#On crée une première liste secondaire qu'on ajoutera à la liste principale 
+liste_secondaire = []
+
+#On additionne les deux listes 
+liste_principale += liste_secondaire 
+
+#Enregistrer les données dans un fichier txt pour ces listes et les suivantes grâce au mode écriture 
